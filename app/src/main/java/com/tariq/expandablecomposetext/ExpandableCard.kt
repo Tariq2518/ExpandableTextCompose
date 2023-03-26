0557bab8ba0671b77be7ab1d56237b055c76c786
tariq2518 <tariq.hussain2518@gmail.com>
2023-03-26 15:48:27 +0500

    Expandable card

--- app/src/main/java/com/tariq/expandablecomposetext/ExpandableCard.kt
+++ app/src/main/java/com/tariq/expandablecomposetext/ExpandableCard.kt
@@ -50,7 +50,7 @@ fun ExpandableCard() {
         onClick = {
             expandedState = !expandedState
         }
-    ) {g
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
